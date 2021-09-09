429bd393cb19e69f1aa153317a35943f4aacaa61
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2021-09-09 07:23:06 +0200

    Fix server ports

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -110,4 +110,4 @@ logging.basicConfig(handlers=[
 
 # start the application on port 7111
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='7111')
+    app.run(host='0.0.0.0', port='3111')
